1be8e2cb6c57596888a98c71a31bd94a963538c5
AnonymousUserOliver <zw1400@nyu.edu>
2019-10-24 17:16:16 -0400

    Implementing skeleton of profile component user-story #59 task #61

--- frontend/src/components/Team-member.js
+++ frontend/src/components/Team-member.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 class Teammember extends Component{
     constructor(props){
       super(props);
@@ -8,9 +9,25 @@ class Teammember extends Component{
   
     render (){
       return (
-       <p>Teammember component will show up here</p>
+      <div>
+        <p>Team member component will show up here</p>
+        <img alt="Profile Picture" src={this.props.imgURL} width = "200" height = "200"/>
+        <h3>Name: {this.props.name}</h3>
+        <h3>Role: {this.props.role}</h3>
+        <p>Bio: {this.props.bio}</p>
+      </div>
       );
-   }
-  }
+    }
+}
+
+Teammember.defaultProps = {
+  name: "Anonumous User", 
+  role: "Classified Role", 
+  bio: "A very private person. ", 
+  imgURL: "default_profile.png"
+  //imgURL: "https://images.unsplash.com/photo-1562813733-b31f71025d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80"
+
+}
+
 
 export default Teammember;
