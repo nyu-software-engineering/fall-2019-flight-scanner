1de72c425d7419346d1f3a2507ecd67a31e8c6a6
AnonymousUserOliver <zw1400@nyu.edu>
2019-10-24 21:29:11 -0400

    Remove constructor user-story#59 task#61

--- frontend/src/components/Team-member.js
+++ frontend/src/components/Team-member.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 
 
 class Teammember extends Component{
-    constructor(props){
-      super(props);
-    }
+    // constructor(props){
+    //   super(props);
+    // }
   
   
     render (){
       return (
       <div>
         <p>Team member component will show up here</p>
-        <img alt="Profile Picture" src={this.props.imgURL} width = "200" height = "200"/>
+        <img alt="Profile" src={this.props.imgURL} width = "200" height = "200"/>
         <h3>Name: {this.props.name}</h3>
         <h3>Role: {this.props.role}</h3>
         <p>Bio: {this.props.bio}</p>
@@ -24,7 +24,7 @@ Teammember.defaultProps = {
   name: "Anonumous User", 
   role: "Classified Role", 
   bio: "A very private person. ", 
-  imgURL: "default_profile.png"
+  imgURL: "ASSETS/default_profile.png"
   //imgURL: "https://images.unsplash.com/photo-1562813733-b31f71025d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80"
 
 }
