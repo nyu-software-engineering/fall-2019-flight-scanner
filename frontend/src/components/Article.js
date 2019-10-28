72eaccec47d38794151264bd3f700c6ae0409f40
Yashodhan Singh <35937088+YashodhanSingh@users.noreply.github.com>
2019-10-28 11:55:02 -0400

    Updating Article.js
    fixed a typo in imgURL

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
 
 class Article extends Component{
-    constructor(props){
-      super(props);
+    //constructor(props){
+    //  super(props);
     }
   
   
     render (){
       return (
-       <p>Article component will show up here</p>
+      <div>
+          <img alt = "Banner" src={this.props.imgURL} width = "900" height = "600"/>
+          <h2>{this.props.title}</h3>
+          <p style = "text-align:left;">
+              By {this.props.author}
+              <span style="float:right;">
+                  {this.props.datePublished}
+              </span>
+          </p>
+          <p>{this.props.content}</p>
+      </div>
       );
-   }
-  }
+    }
+}
 
+Article.defaultProps = {
+    title: "Default Title", 
+    author: "Someone", 
+    datePublised: "12/12/2020",
+    content: "Default content"
+    imgURL: "ASSETS/defaultBanner.png"
+    
+}
 export default Article;
