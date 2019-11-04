0fcbc771a5a8c8f9823fe618b29333f6f9c69679
Nav <navyasuri@nyu.edu>
2019-11-03 21:47:51 -0500

    Refactor Article component to use material

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-class Article extends Component{
-    
-    render (){
-      return (
-      <div>
-          <img alt = "Banner" src={this.props.imgURL} width = "900" height = "600"/>
-          <h1>{this.props.title}</h1>
-          <h4>By {this.props.author}</h4>
-          <h4>{this.props.datePublised}</h4>
-          <p>{this.props.content}</p>
-      </div>
-      );
+const useStyles = makeStyles({
+    page: {
+        width: "100%",
+        maxWidth: "720"
+    },
+    bannerImage: {
+        width: "100%"
+    },
+    title: {
+        textTransform: "uppercase"
+    }
+});
+
+const ArticleComponent = (props) => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.page}>
+            <Typography variant="h2" className={classes.title}>
+                {props.title}
+            </Typography>
+            <div className='bannerImage' src={props.banner}>
+                {props.banner}
+            </div>
+        </div>
+    )
+}
+
+class Article extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <ArticleComponent props={this.props}/>
+        )
     }
 }
 
 Article.defaultProps = {
-    title: "Default Title",
-    author: "Someone",
-    datePublised: "12/12/2020",
-    content: "Default Full Article content",
-    imgURL: "ASSETS/defaultBanner.png"
-
+    banner: "ASSETS/defaultBanner.png",
+    title: "Dummy Title",
+    teaser: "A teaser for the dummy article here",
+    authors: [
+        "Dummy Author 1",
+        "Dummy Author 2"
+    ],
+    body: "Some dummy markdown here"
 }
-export default Article;
 
+export default Article
