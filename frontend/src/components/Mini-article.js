602ab77c5fff2c7847a920f5e047891438b8c0a5
Yashodhan Singh <35937088+YashodhanSingh@users.noreply.github.com>
2019-11-05 17:59:54 -0500

    user-story/41/task/109/Using-Material-UI-for-Miniarticle-Component
    updating miniarticle.js using grid

--- frontend/src/components/Mini-article.js
+++ frontend/src/components/Mini-article.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow:1,
+        textAlign: "left"
+    },
+    control: {
+      padding:theme.spacing(2)
+    },
+    page: {
+        display: "inline-block",
+        width: "100%",
+        maxWidth:"1080px"
+    },
+    bannerImage: {
 
-class Miniarticle extends Component {
-  
-  render() {
+    },
+    title:{
+        textTransform: "uppercase",
+        fontWeight: "400",
+        textAlign: "left"
+    },
+    teaser: {
+        textAlign: "left",
+        fontStyle: "italic"
+    },
+    author: {
+        textAlign: "left"
+    },
+}));
+
+const MiniArticleComponent = (props) => {
+    const classes = useStyles()
+    props = props.props
     return (
-      <div>
-          <img alt = "Banner" src={this.props.imgURL} width = "300" height = "200"/>
-          <h1>{this.props.title}</h1>
-          <h4>By {this.props.author}</h4>
-          <h4>{this.props.datePublised}</h4>
-          <p>{this.props.content}</p>
+      <div className={classes.page}>
+        <Grid container className = {classes.root} spacing = {2}>
+          <Grid item xs={12}>
+            <Grid container justify = "center" spacing = {10}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(value => (
+                <Grid key ={value} item>
+                <Box
+                  width={300}
+                  lineHeight={1}
+                  float="left">
+                  <img className= 'bannerImage' alt ="banner for mini article component" src = {props.banner} style = {{ maxWidth: "300px"}}/>
+                  <Typography variant="h6" className = {classes.title}>
+                      {props.title}
+                  </Typography>
+                  <Typography variant="h7" className = {classes.author} style = {{ fontWeight: "200"}}>
+                      {props.author}
+                  </Typography>
+                  <Typography variant="subtitle1" className = {classes.teaser}>
+                      {props.teaser}
+                  </Typography>
+                </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
-    );
-  }
+    )
 }
 
-Miniarticle.defaultProps = {
-    title: "Default Title",
-    author: "Someone",
-    datePublised: "12/12/2020",
-    content: "Default Mini Article content",
-    imgURL: "ASSETS/defaultBanner.png"
+class MiniArticle extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
 
+    render() {
+        return (
+            <MiniArticleComponent props = {this.props} />
+        )
+    }
 }
 
-export default Miniarticle;
+MiniArticle.defaultProps = {
+    banner: "ASSETS/trumpCartoon.jpg",
+    title: "Dummy Title goes here",
+    teaser: "A teaser for the dummy article here and even more teaser",
+    author: "Dummy Author 1"
+}
+
+export default MiniArticle
