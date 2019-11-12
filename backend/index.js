1d8301bb85bebe4a4de2aabea1b48b4ab7fa1540
Abdullah Zameek <arz268@nyu.edu>
2019-11-12 15:12:39 -0500

    minor

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const articlesRouter = require('./routes/article');
+app.use('/article', articlesRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
