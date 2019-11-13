45aa5304f80c36e2ad7c0a96e487be71be062aaa
Nav <navyasuri@nyu.edu>
2019-11-13 12:31:36 -0500

    Added changes to header class

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,54 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
-export default class Header extends Component{
 
-    constructor(props){
+export default class Header extends Component {
+
+    constructor(props) {
         super(props)
-        this.state = {categories: []}
+        this.state = { categories: [] }
+        this.styles = makeStyles({
+            nav: {
+
+            },
+            container: {
+
+            },
+            nav_item: {
+
+            },
+            nav_link: {
+                textTransform: "uppercase",
+                fontFamily: "sans-serif"
+            },
+
+        })
     }
 
     componentDidMount() {
-        axios.get("")
-        .then(response => {
-            this.setState({categories: response.data})
-        })
-        .catch(error => {
-            console.log("Error! ", error)
-        })
+        axios.get("http://10.253.105.223:5000/category/getAllCategories")
+            .then(response => {
+                this.setState({
+                    categories: response.data.forEach((cat, id) => {
+                        return {
+                            _id: id, 
+                            category: cat
+                        }
+                    })
+                })
+            })
+            .catch(error => {
+                console.log("Error! ", error)
+            })
     }
 
     loadCategories = (categories, styles) => {
-        return categories.map(category => {
+        return categories.map((key, category) => {
+            console.log(key, category)
             return (<li className={styles.nav_item}>
-                <Link className={styles.nav_link} to={`/${category}`}>{category}</Link>
+                <Link className={styles.nav_link} to={`/${category}`} key={key}>{category}</Link>
             </li>)
         })
     }
 
-    styles = makeStyles({
-        nav: {
-
-        },
-        container: {
-
-        },
-        nav_item: {
-
-        },
-        nav_link: {
-            textTransform: "uppercase",
-            fontFamily: "sans-serif"
-        },
-
-    })
-
     render() {
         return (
-            <nav className={styles.nav}>
-                <div className={styles.container}>
-                    <ul className="Nav__item-wrapper"> {loadCategories(tempCategories, styles)} </ul>
+            <nav className={this.styles.nav}>
+                <div className={this.styles.container}>
+                    <ul className="Nav__item-wrapper"> {this.loadCategories(this.state.categories, this.styles)} </ul>
                 </div>
             </nav>
         );
