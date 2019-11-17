690139cbc9119b639b00c563c8c60e822b7d0e5d
AnonymousUserOliver <zw1400@nyu.edu>
2019-11-17 17:08:09 -0500

    implement Account page

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -8,23 +8,35 @@ import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
 
 
 
 const styles = theme => ({
-    card: {
+    root: {
+        width: '100%',
+    },
+
+    cardPic: {
         marginLeft: '5%',
-        maxWidth: 345,
-		maxHeight: 800,
+        width: '20%',
+		height: '370px',
 		margin: "20px",
 
       },
-    media: {
-		height: 260,
+    cardBio: {
+        width: '60%',
+        height: '370px',
+        marginRight: '5%',
+        marginTop: '20px'
     },
+    // media: {
+	// 	height: '270px',
+    // },
     button: {
-        // marginTop: theme.spacing(5),
-        // marginBottom: theme.spacing(2),
+        marginTop: theme.spacing(3),
+        // marginBottom: theme.spacing(1),
         // marginLeft: theme.spacing(1),
         // marginRight: theme.spacing(1),
         background: '#2E3B55',
@@ -35,8 +47,47 @@ const styles = theme => ({
     },
 
     cardActions: {
-        justifyContent: 'center'
+        justifyContent: 'center',
+
     },
+
+    box: {
+        justifyContent: 'space-between'
+    },
+
+    inputbox: {
+        width: '90%',
+        height: '270px',
+
+        marginTop: '20px'
+    },
+
+    bioActionArea:{
+        height: '100px'
+    },
+
+    cardActionArea: {
+        maxHeight: "300px",
+        minHeight: "300px"
+    },
+    updatePictureButton:{
+
+        background: '#2E3B55',
+        color: 'white',
+        '&:hover': {
+            background: '#586481',
+        },
+    },
+
+    nameAndRole: {
+        marginTop: theme.spacing(1),
+    },
+
+    bioText: {
+        maxHeight: '300px'
+    }
+
+
 
     // preview: {
     //     marginTop: theme.spacing(5),
@@ -73,15 +124,25 @@ class Account extends Component{
         alert("updatePicture function called"); 
     }
 
+    updateBio= () =>{
+        alert("updateBio function called"); 
+    }
+
+    showPreviews= () => {
+        alert("showPreviews function called")
+    }
+
     render(){
         const {classes} = this.props; 
         return(
-            <div>
-
+            <div class="flex-container" className={classes.root}>
+                
                 <Typography gutterBottom variant="h5" component="h2">
                     Customize your profile visible to readers!
                 </Typography>
-                <Card className={classes.card}>
+
+                <Box display='flex' p={1} className={classes.box}>
+                <Card className={classes.cardPic}>
                 <CardActionArea>
                     <CardMedia
                     component="img"
@@ -99,8 +160,72 @@ class Account extends Component{
                 </CardActions>
                 </Card>
 
+                <Card className={classes.cardBio}>
+                <CardActionArea >
+                <TextField
+                    id="Bio"
+                    label="Bio"
+                    rows='11'
+                    variant="outlined"
+                    multiline
+                    
+                    className={classes.inputbox}
+
+                />
+
+                </CardActionArea>
+                <CardActions className={classes.cardActions}>
+
+                    <Button  onClick={this.updateBio} className={classes.updatePictureButton}>
+                    Update Bio
+                    </Button>
+
+                </CardActions>
+                </Card>
+                </Box>
 
 
+
+                <Box display='flex' p={1} className={classes.box}>
+                <Card className={classes.cardPic}>
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    alt="Admin Profile Picture"
+                    image="./ASSETS/default_profile.png"
+                    title="Admin Profile Picutre"
+                    />
+
+                </CardActionArea>
+                <CardActions className={classes.cardActions}>
+                <Typography gutterBottom variant="h5" component="h2" className={classes.nameAndRole}>
+                    Author Name
+                    <br/>
+                    Author Role
+                </Typography>
+
+                </CardActions>
+                </Card>
+
+                <Card className={classes.cardBio}>
+                <CardActionArea className = {classes.cardActionArea}>
+                <Typography gutterBottom variant="h5" component="h2" className={classes.bioText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus dui quis sapien lacinia eleifend. Nulla ullamcorper magna a massa ornare fringilla. Nunc ut tortor non purus sollicitudin vehicula ac non sapien. Ut nunc risus, eleifend non tortor non, auctor sagittis turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed faucibus purus et enim facilisis, vel congue justo venenatis. Proin egestas, purus in ultricies tincidunt, orci mi feugiat leo, id pharetra elit erat sed nisi. Donec ipsum est, lobortis quis tortor at, facilisis euismod risus. Praesent pretium mauris ut neque tempor interdum. Nullam imperdiet ex sed facilisis fringilla.
+                </Typography>
+
+                </CardActionArea>
+                <CardActions className={classes.cardActions}>
+
+                   <Button  onClick={this.updateBio} className={classes.updatePictureButton}>
+                    Update Bio
+                    </Button> 
+
+                </CardActions>
+                </Card>
+                </Box>
+                <Button  onClick={this.showPreviews} className={classes.updatePictureButton}>
+                    Show Previews
+                </Button> 
             </div>
         ); 
     }
