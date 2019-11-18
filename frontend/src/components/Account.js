32e0b87c194a614745df3793ea213542a6de3673
AnonymousUserOliver <zw1400@nyu.edu>
2019-11-17 20:55:38 -0500

    upload button

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-// import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import ImageUploader from 'react-images-upload';
+
 
 
 
@@ -34,17 +34,12 @@ const styles = theme => ({
         marginTop: '20px', 
         boxShadow: "none"
     },
-    // media: {
-	// 	height: '270px',
-    // },
+
     button: {
-        // marginTop: "10%",
-        // marginBottom: "20%",
-        // marginLeft: theme.spacing(1),
-        // marginRight: theme.spacing(1),
+
         background: '#2E3B55',
         color: 'white',
-        // height: "20%",
+
         '&:hover': {
             background: '#586481',
         },
@@ -83,48 +78,24 @@ const styles = theme => ({
         },
     },
 
-    // nameAndRole: {
-    //     marginTop: theme.spacing(1),
-    // },
 
-    // bioText: {
-    //     maxHeight: '300px'
-    // }
-
-
-
-    // preview: {
-    //     marginTop: theme.spacing(5),
-    //     marginBottom: theme.spacing(2),
-    //     marginLeft: theme.spacing(1),
-    //     marginRight: theme.spacing(1),
-    //     background: '#2E3B55',
-    //     color: 'white',
-    //     '&:hover': {
-    //         background: '#586481',
-    //     },
-    // },
-    // deleteB: {
-    //     marginTop: theme.spacing(5),
-    //     marginBottom: theme.spacing(2),
-    //     marginLeft: theme.spacing(1),
-    //     marginRight: theme.spacing(1),
-    //     background: '#93160d',
-    //     color: 'white',
-    //     '&:hover': {
-    //         background: '#ca4b35',
-    //     }
-
-
-    // },
 })
 
 class Account extends Component{
     constructor(props){
         super(props); 
         this.state = {
-
+            bio: '', 
+            imgURL: '',
+            pictures: [],
+            onDrop:  this.onDrop.bind(this)
         }
+    }
+
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
     }
 
     updatePicture = () =>{
@@ -133,11 +104,17 @@ class Account extends Component{
 
     updateBio= () =>{
         alert("updateBio function called"); 
+        alert(this.state.bio); 
     }
 
-    // showPreviews= () => {
-    //     alert("showPreviews function called")
-    // }
+    handleChange = (event) => {
+        this.setState({
+            bio: event.target.value
+        })
+        
+    }
+
+
 
     render(){
         const {classes} = this.props; 
@@ -158,13 +135,9 @@ class Account extends Component{
                     title="Admin Profile Picutre"
                     />
                 </CardActionArea>
-                <CardActions className={classes.cardActions}>
-                    <Button onClick={this.updatePicture} className={classes.button}>
-                    Update Picture
-                    </Button>
 
-                </CardActions>
                 </Card>
+
 
                 <Card className={classes.cardBio}>
                 <CardActionArea >
@@ -174,7 +147,7 @@ class Account extends Component{
                     rows='11'
                     variant="outlined"
                     multiline
-                    
+                    onChange={this.handleChange}
                     className={classes.inputbox}
 
                 />
@@ -190,49 +163,17 @@ class Account extends Component{
                 </Card>
                 </Box>
 
-
-
-                {/* <Box display='flex' p={1} className={classes.box}> */}
-                {/* <Card className={classes.cardPic}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    alt="Admin Profile Picture"
-                    image="./ASSETS/default_profile.png"
-                    title="Admin Profile Picutre"
-                    />
-
-                </CardActionArea>
-                <CardActions className={classes.cardActions}>
-                <Typography gutterBottom variant="h5" component="h2" className={classes.nameAndRole}>
-                    Author Name
-                    <br/>
-                    Author Role
-                </Typography>
-
-                </CardActions>
-                </Card> */}
-
-                {/* <Card className={classes.cardBio}>
-                <CardActionArea className = {classes.cardActionArea}>
-                <Typography gutterBottom variant="h5" component="h2" className={classes.bioText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus dui quis sapien lacinia eleifend. Nulla ullamcorper magna a massa ornare fringilla. Nunc ut tortor non purus sollicitudin vehicula ac non sapien. Ut nunc risus, eleifend non tortor non, auctor sagittis turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed faucibus purus et enim facilisis, vel congue justo venenatis. Proin egestas, purus in ultricies tincidunt, orci mi feugiat leo, id pharetra elit erat sed nisi. Donec ipsum est, lobortis quis tortor at, facilisis euismod risus. Praesent pretium mauris ut neque tempor interdum. Nullam imperdiet ex sed facilisis fringilla.
-                </Typography>
-
-                </CardActionArea>
-                <CardActions className={classes.cardActions}>
-
-                   <Button  onClick={this.updateBio} className={classes.updatePictureButton}>
-                    Update Bio
-                    </Button> 
-
-                </CardActions>
-                </Card> */}
-                {/* </Box> */}
-                {/* <Button  onClick={this.showPreviews} className={classes.updatePictureButton}>
-                    Show Previews
-                </Button>  */}
+                <ImageUploader
+                withIcon={false}
+                withLabel={false}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
             </div>
+
+            
         ); 
     }
 }
