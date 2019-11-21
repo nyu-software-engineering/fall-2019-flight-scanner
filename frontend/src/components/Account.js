212d89e26af58a3ea563c5d6bd2e5364d97d55d9
AnonymousUserOliver <zw1400@nyu.edu>
2019-11-21 14:23:52 -0500

    fix image button styling and upload function

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -3,7 +3,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import ImageUploader from 'react-images-upload';
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container';
 import Profile from './Teammember-profile'; 
@@ -20,10 +19,6 @@ const styles = theme => ({
 
     pic: {
         width: "100%", 
-    },
-
-    updateBio: {
-        // verticalAlign: "middle"
     },
 
     button: {
@@ -59,21 +54,35 @@ class Account extends Component{
         super(props); 
         this.state = {
             bio: 'Welcome to your complete guide to the best Chow Chow names, giving you plenty of ideas for what to call your beautiful and powerful Chow Chow. Chow Chow dog names should highlight this ancient Chinese breed’s best qualities. Be that their gorgeous mane of red, white, blue, cream, blue, or tawny fur; their legendary “tough dog” presence; or perhaps their Chinese roots! No matter what inspires your potential dog names for Chow Chow, there are some things to keep in mind to ensure that your pooch both knows and answers to their name.', 
-            imgURL: '',
-            pictures: [],
-            onDrop:  this.onDrop.bind(this), 
+            imgURL: 'http://lorempixel.com/300/300/',
             tempBio: ""
         }
     }
 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
+    isUriImage = (uri) => {
+        //make sure we remove any nasty GET params 
+        uri = uri.split('?')[0];
+        //moving on, split the uri into parts that had dots before them
+        var parts = uri.split('.');
+        //get the last part ( should be the extension )
+        var extension = parts[parts.length-1];
+        //define some image types to test against
+        var imageTypes = ['jpg','jpeg','tiff','png','gif','bmp'];
+        //check if the extension matches anything in the list.
+        if(imageTypes.indexOf(extension) !== -1) {
+            return true;   
+        }
+        return false; 
     }
 
+
     updatePicture = () =>{
-        alert("updatePicture function called"); 
+        let newImg = prompt("Please enter image URL: "); 
+        while(!this.isUriImage(newImg)){
+            newImg = prompt("Invalid URL, Please enter again: "); 
+        }
+        this.setState({imgURL: newImg}); 
+        
     }
 
     updateBio= () =>{
@@ -107,15 +116,10 @@ class Account extends Component{
                 </Typography>
                 <Grid container spacing={6}>
                     <Grid item xs={12} sm={4}>
-                    <img alt={this.props.name} className={classes.pic} src={this.props.picture}></img>
-                    <ImageUploader
-                        withIcon={false}
-                        withLabel={false}
-                        buttonText='Choose images'
-                        onChange={this.onDrop}
-                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                        maxFileSize={5242880}
-                    />
+                    <img alt={this.props.name} className={classes.pic} src={this.state.imgURL}></img>
+                    <Button  onClick={this.updatePicture} className={classes.updatePictureButton}>
+                                Update Picture
+                            </Button>
                     </Grid>
                     <Grid item xs={12} sm={8}>
                         <div className={classes.updateBio}>
@@ -136,7 +140,7 @@ class Account extends Component{
                     </Grid>
 
                 </Grid>
-                <Profile picture={this.state.picture} bio={this.state.bio}></Profile>
+                <Profile picture={this.state.imgURL} bio={this.state.bio}></Profile>
 
             </Container>
 
