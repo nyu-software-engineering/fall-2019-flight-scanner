ee64e6323dd04ce62771f56fc8f3ac20d9f78ae9
Nav <navyasuri@nyu.edu>
2019-11-24 17:10:27 -0500

    Added article routing via slug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Article from './components/Article';
 import Admin from './components/Admin'
 import Category from './components/Category';
 import Create from './components/Create';
-import Account from './components/Account'; 
+import Account from './components/Account';
 import Profile from './components/Teammember-profile';
 import MiniArticle from './components/Mini-article';
 
@@ -25,7 +25,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Router>
-					<NavRoute exact path="/" component={Article} />
+					<NavRoute exact path="/" component={MiniArticle} />
 					<NavRoute exact path="/category/:id" component={Category} />
 					<Route exact path="/admin" component={Admin} />
 					<Route exact path="/admin/create" component={Create} />
@@ -33,10 +33,7 @@ class App extends Component {
 					<NavRoute exact path="/profile" component={Profile} />
 					<NavRoute exact path="/samplemini" component={MiniArticle} />
 					<NavRoute exact path="/account" component={Account} />
-					{/* <Route exact path="/" component={Article} />
-					<Route exact path="/category/:id" component={Category} />
-					<Route exact path="/admin" component={Admin} />
-					<Route exact path="/sampleArticle" component={Article} /> */}
+					<NavRoute exact path="/article/:id" component={Article} />
 				</Router>
 			</div>
 		);
