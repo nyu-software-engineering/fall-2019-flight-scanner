0ecd3a062849a46f17eb23b68e5b5a4049048e10
AnonymousUserOliver <zw1400@nyu.edu>
2019-12-02 12:58:33 -0500

    makes changes as requested

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,9 +3,7 @@ import Miniarticle from './Mini-article';
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Teammember from './Team-member'
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+
 
 const styles = theme => ({
     root: {
@@ -20,10 +18,6 @@ const styles = theme => ({
         // maxWidth: '1000px'
     },
 
-    paper: {
-        padding: theme.spacing(3, 2), 
-
-    },
 
 
 })
@@ -38,12 +32,7 @@ class Landing extends Component{
         return(
             
             <div className={classes.paper}>
-                <Paper className={classes.root}>
-                    <Typography variant="h1" component="h3">
-                        Welcome to LightShare
-                    </Typography>
 
-                </Paper>
 
                 <Container className={classes.container}>
                     <Grid container spacing={3}>
@@ -55,7 +44,7 @@ class Landing extends Component{
 
                         </Grid>
                         <Grid item xs={6} sm={3}>
-                            <Teammember></Teammember>
+                            <Miniarticle></Miniarticle>
 
                         </Grid>
                         <Grid item xs={6} sm={6}>
@@ -66,18 +55,18 @@ class Landing extends Component{
                             <Miniarticle ></Miniarticle>
                         </Grid>
                         <Grid item xs={6} sm={3}>
-                            <Teammember></Teammember>
+                            <Miniarticle></Miniarticle>
 
                         </Grid>
                         <Grid item xs={6} sm={3}>
                             <Miniarticle  ></Miniarticle>
 
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Miniarticle  ></Miniarticle>
                         </Grid>
                         <Grid item xs={6} sm={6}>
-                            <Miniarticle  ></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Teammember></Teammember>
+                            <Miniarticle></Miniarticle>
                         </Grid>
 
                     </Grid>
@@ -85,13 +74,6 @@ class Landing extends Component{
             </div>
         )
     }
-}
-
-Landing.defaultProps = {
-    picture: 'http://lorempixel.com/300/300/',
-    name: 'Author Author',
-    role: 'Chief Editor',
-    bio: "Welcome to your complete guide to the best Chow Chow names, giving you plenty of ideas for what to call your beautiful and powerful Chow Chow. Chow Chow dog names should highlight this ancient Chinese breed’s best qualities. Be that their gorgeous mane of red, white, blue, cream, blue, or tawny fur; their legendary “tough dog” presence; or perhaps their Chinese roots! No matter what inspires your potential dog names for Chow Chow, there are some things to keep in mind to ensure that your pooch both knows and answers to their name.",
 }
 
 
