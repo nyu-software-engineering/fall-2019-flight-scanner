1faf5a3b99e2724fc2be8b90fbd45b896d212676
AnonymousUserOliver <zw1400@nyu.edu>
2019-12-03 22:47:27 -0500

    populate miniarticles from database

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,6 +3,7 @@ import Miniarticle from './Mini-article';
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
 
 
 const styles = theme => ({
@@ -24,10 +25,41 @@ const styles = theme => ({
 
 class Landing extends Component{
 
+    constructor(props) {
+        super(props)
+        // To add in some default articles for when DB does not provide any, add them to the Array Below
+        this.state = {
+            articles: [],
+
+
+        }
+    }
+
+
+    componentDidMount() {
+        axios.get(`http://localhost:5000/article`)
+            .then(response => {
+
+                console.log("didmount", response.data)
+                this.setState({
+                    articles: response.data,
+
+                })
+            })
+            .catch(error => {
+                console.log("ERROR in Category loading ", error)
+            })
+
+
+    }
+
+
+
     
 
     render(){
         const { classes }  = this.props; 
+        
         return(
             
             <div className={classes.paper}>
@@ -35,79 +67,75 @@ class Landing extends Component{
 
                 <Container className={classes.container}>
                     <Grid container spacing={6}>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle ></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
 
+                    {this.state.articles.slice(2,6).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={3} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
                         </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
+                    )}
+                    {this.state.articles.slice(0,1).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={9} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
+                        </Grid>
+                    )}
+                    {this.state.articles.slice(1,2).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={3} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
+                        </Grid>
+                    )}
 
+                    {this.state.articles.slice(6,10).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={3} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
                         </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle  ></Miniarticle>
+                    )}
+                    {this.state.articles.slice(10,13).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={2} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
+                        </Grid>
+                    )}
 
+                    {this.state.articles.slice(13,15).map(article =>
+                        // <p>{article.articleText}</p>
+                        <Grid item xs={6} sm={3} key={article.articleId}>
+                            <Miniarticle banner={article.articleImg}
+                                teaser={article.articleTeaser}
+                                title={article.articleTitle}
+                                author={article.articleAuthor}
+                                slug={article.articleId}
+                            />
                         </Grid>
-                        <Grid item xs={6} sm={9}>
-                            <Miniarticle ></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle  ></Miniarticle>
-
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle  ></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={2}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
-
-                        <Grid item xs={6} sm={3}>
-                            <Miniarticle></Miniarticle>
-                        </Grid>
+                    )}
 
                     </Grid>
                 </Container>
