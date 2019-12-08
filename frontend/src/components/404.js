5487dd982968250cb67e96e654be2e46e1db173c
KertuKoss <kk3609@nyu.edu>
2019-12-07 23:06:15 -0500

    link to landing page

--- frontend/src/components/404.js
+++ frontend/src/components/404.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button'
 
 
 class ErrorPage extends Component {
@@ -8,6 +10,7 @@ class ErrorPage extends Component {
             <div>
             <img style = {{marginTop: '10%', marginBottom: '10px'}} src={'https://coverager.com/wp-content/uploads/2017/02/oh-no.jpg'} alt={'Man in shock'}></img>
             <h2> Are you sure you are looking for the right page? </h2>
+            <Button><Link style ={{color:'black', textDecoration: 'none'}} to='/landing'> Travel back to Homepage</Link></Button>
             </div>
         );
     }
