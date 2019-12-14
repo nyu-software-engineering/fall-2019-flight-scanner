560dbf19b335f34a2bc83dab3e432ce9e53111d1
Nav <navyasuri@nyu.edu>
2019-12-14 02:36:05 -0500

    Critical import of css

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -7,19 +7,20 @@ import Create from './components/Create';
 import Account from './components/Account';
 import Management from './components/Management';
 import Progress from './components/Progress';
+import './App.css'
 
-const Refresh = ({ path = '/' }) => (
-    <Route
-        path={path}
-        component={({ history, location, match }) => {
-            history.replace({
-                ...location,
-                pathname: location.pathname.substring(match.path.length)
-            });
-            return null;
-        }}
-    />
-);
+// const Refresh = ({ path = '/' }) => (
+//     <Route
+//         path={path}
+//         component={({ history, location, match }) => {
+//             history.replace({
+//                 ...location,
+//                 pathname: location.pathname.substring(match.path.length)
+//             });
+//             return null;
+//         }}
+//     />
+// );
 
 class App extends Component {
     render() {
